34db8cf9d53d87bc2c11abcaae9706d14d0c7e59
Alex Sandro <alex.sandro.1199@gmail.com>
2023-02-21 16:42:01 -0300

    Conectando ao MongoDB com repository e service

---
+++ src/main/java/com/alex/worshopmongo/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package com.alex.worshopmongo.repository;
+
+import com.alex.worshopmongo.domain.User;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends MongoRepository<User, String> {
+}
